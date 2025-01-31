fda01de8d52bb1ac47caa3576b7656bc559eaf85
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-01-31 18:54:00 +0300

    animattion

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import Hero from './components/Hero';
-// import About from './components/About';
-// import Skills from './components/Skills';
-// import Projects from './components/Projects';
-// import Contact from './components/Contact';
+// import React from 'react';
+// import Hero from './components/Hero';
+// // import About from './components/About';
+// // import Skills from './components/Skills';
+// // import Projects from './components/Projects';
+// // import Contact from './components/Contact';
 
-const App = () => {
-  return (
-    <div>
-      <Hero />
-      {/* <About />
-      <Skills />
-      <Projects />
-      <Contact /> */}
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div>
+//       <Hero />
+//       <Skills />
+//       <Projects />
+//       <Contact /> */}
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
